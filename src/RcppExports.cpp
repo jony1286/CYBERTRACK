9a84242c4cd43a080ba171681feb1781e65e9235
Kodai Minoura <kodaiminoura@KodainoMacBook-Pro.local>
2020-03-21 13:26:42 +0900

    debugging

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -202,49 +202,6 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
-// rcpparma_hello_world
-arma::mat rcpparma_hello_world();
-RcppExport SEXP _CYBERTRACK_rcpparma_hello_world() {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    rcpp_result_gen = Rcpp::wrap(rcpparma_hello_world());
-    return rcpp_result_gen;
-END_RCPP
-}
-// rcpparma_outerproduct
-arma::mat rcpparma_outerproduct(const arma::colvec& x);
-RcppExport SEXP _CYBERTRACK_rcpparma_outerproduct(SEXP xSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< const arma::colvec& >::type x(xSEXP);
-    rcpp_result_gen = Rcpp::wrap(rcpparma_outerproduct(x));
-    return rcpp_result_gen;
-END_RCPP
-}
-// rcpparma_innerproduct
-double rcpparma_innerproduct(const arma::colvec& x);
-RcppExport SEXP _CYBERTRACK_rcpparma_innerproduct(SEXP xSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< const arma::colvec& >::type x(xSEXP);
-    rcpp_result_gen = Rcpp::wrap(rcpparma_innerproduct(x));
-    return rcpp_result_gen;
-END_RCPP
-}
-// rcpparma_bothproducts
-Rcpp::List rcpparma_bothproducts(const arma::colvec& x);
-RcppExport SEXP _CYBERTRACK_rcpparma_bothproducts(SEXP xSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< const arma::colvec& >::type x(xSEXP);
-    rcpp_result_gen = Rcpp::wrap(rcpparma_bothproducts(x));
-    return rcpp_result_gen;
-END_RCPP
-}
 
 static const R_CallMethodDef CallEntries[] = {
     {"_CYBERTRACK_softmax", (DL_FUNC) &_CYBERTRACK_softmax, 1},
@@ -260,10 +217,6 @@ static const R_CallMethodDef CallEntries[] = {
     {"_CYBERTRACK_sigma_update_v2", (DL_FUNC) &_CYBERTRACK_sigma_update_v2, 7},
     {"_CYBERTRACK_rowSums", (DL_FUNC) &_CYBERTRACK_rowSums, 1},
     {"_CYBERTRACK_cybertrack", (DL_FUNC) &_CYBERTRACK_cybertrack, 12},
-    {"_CYBERTRACK_rcpparma_hello_world", (DL_FUNC) &_CYBERTRACK_rcpparma_hello_world, 0},
-    {"_CYBERTRACK_rcpparma_outerproduct", (DL_FUNC) &_CYBERTRACK_rcpparma_outerproduct, 1},
-    {"_CYBERTRACK_rcpparma_innerproduct", (DL_FUNC) &_CYBERTRACK_rcpparma_innerproduct, 1},
-    {"_CYBERTRACK_rcpparma_bothproducts", (DL_FUNC) &_CYBERTRACK_rcpparma_bothproducts, 1},
     {NULL, NULL, 0}
 };
 
